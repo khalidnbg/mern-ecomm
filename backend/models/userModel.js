d532305f2f8be25850eb5f664eab9bff5af7c65e
khalidnbg <khalidnabgaoui4@gmail.com>
2024-04-29 19:07:44 +0100

    set up the model structure for produc

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -2,49 +2,54 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const { ObjectId } = mongoose.Schema;
 
-const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a name"],
-  },
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a name"],
+    },
 
-  email: {
-    type: String,
-    required: [true, "Please add an email"],
-    unique: true,
-    trim: true,
-    match: [
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      "Please add valid email",
-    ],
-  },
+    email: {
+      type: String,
+      required: [true, "Please add an email"],
+      unique: true,
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please add valid email",
+      ],
+    },
 
-  password: {
-    type: String,
-    resuired: [true, "Please enter your password"],
-    minLength: [6, "Password must be at least 6 characters"],
-  },
+    password: {
+      type: String,
+      resuired: [true, "Please enter your password"],
+      minLength: [6, "Password must be at least 6 characters"],
+    },
 
-  role: {
-    type: String,
-    required: [true],
-    default: "customer",
-    enum: ["customer", "admin"],
-  },
+    role: {
+      type: String,
+      required: [true],
+      default: "customer",
+      enum: ["customer", "admin"],
+    },
 
-  photo: {
-    type: String,
-    required: [true, "Please add a photo"],
-    default: "https://i.ibb.co/4pDnDk1/avatar.png",
-  },
+    photo: {
+      type: String,
+      required: [true, "Please add a photo"],
+      default: "https://i.ibb.co/4pDnDk1/avatar.png",
+    },
 
-  phone: {
-    type: String,
-    default: "+000 000 ...",
-  },
+    phone: {
+      type: String,
+      default: "+000 000 ...",
+    },
 
-  address: { type: Object },
-});
+    address: { type: Object },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Hash paswword before saving to database
 userSchema.pre("save", async function (next) {
