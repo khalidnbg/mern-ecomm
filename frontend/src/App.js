9d7215f461f9b8a91613d1b6e95faabf770a4329
khalidnbg <khalidnabgaoui4@gmail.com>
2024-04-29 15:41:21 +0100

    set up the header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/home/Home";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
-import axios from "axios";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getLoginStatus } from "./redux/features/auth/authSlice";
+
 const App = () => {
   axios.defaults.withCredentials = true;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getLoginStatus());
+  }, [dispatch]);
 
   return (
     <>
